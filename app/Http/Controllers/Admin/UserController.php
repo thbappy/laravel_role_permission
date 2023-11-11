97a6cda50e2d05ce010583f7f8e22ade8f271357
Tanbeer Hasan <33245907+thbappy@users.noreply.github.com>
2023-11-11 11:40:45 +0600

    Update UserController.php

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -77,7 +77,6 @@ class UserController extends Controller
         $this->validate($request,[
             'name' => 'required',
             'email' => 'required|email|unique:users,email,'.$id,
-            'address' => 'required',
             'role' => 'required',
         ]);
 
